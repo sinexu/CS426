4ada1c9626ae3749d486edd5f757efc7f1643a25
sinexu <sx8@uw.edu>
2016-09-13 16:47:52 -0400

    sine: graph.cpp 5-8 functions

--- cs426_graph_server/graph/graph.cpp
+++ cs426_graph_server/graph/graph.cpp
@@ -10,11 +10,12 @@ uint64_t graph::remove_node(uint64_t node_id){
 uint64_t graph::remove_edge(uint64_t node_a_id, uint64_t node_b_id){
 
 }
-uint64_t graph::get_node(uint64_t node_id){
-
+bool graph::get_node(uint64_t node_id){
+//    return nodes.enum_keys().contains(node_id);
+    retun nodes.find(node_id) != nodes.end;
 }
 uint64_t graph::get_edge(uint64_t node_a_id, uint64_t node_b_id){
-
+    
 }
 uint64_t graph::get_neighbors(uint64_t node_id){
 
--- cs426_graph_server/graph/graph.h
+++ cs426_graph_server/graph/graph.h
@@ -1,27 +1,28 @@
 #pragma once
-#include "../libraray/mongoose.h"
+#include "../library/mongoose.h"
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <utility>
 
 using namespace std;
 
 struct node{
 	uint64_t id;
-	// pair = (neighbor node, edge length)
-	vector<nodex*> neighbors;
-	node(uint64_t i){id=i}
+	vector<node*> neighbors;
+    node(uint64_t i){
+        this->id=i;
+    }
 };
 
 class graph{
-private:
-	unordered_map<uint64_t, node*> nodes;
+public:
+    unordered_map<uint64_t, node*> nodes;
 public:
 	uint64_t add_node(uint64_t node_id);
 	uint64_t add_edge(uint64_t node_a_id, uint64_t node_b_id);
 	uint64_t remove_node(uint64_t node_id);
 	uint64_t remove_edge(uint64_t node_a_id, uint64_t node_b_id);
-	uint64_t get_node(uint64_t node_id);
+	bool get_node(uint64_t node_id);
 	uint64_t get_edge(uint64_t node_a_id, uint64_t node_b_id);
 	uint64_t get_neighbors(uint64_t node_id);
 	uint64_t shortest_path(uint64_t node_a_id, uint64_t node_b_id);
