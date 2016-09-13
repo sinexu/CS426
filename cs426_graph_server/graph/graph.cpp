0978acf4f845a8229dcfb88118981b9d99054bdd
sinexu <sx8@uw.edu>
2016-09-13 17:27:06 -0400

    sine 567

--- cs426_graph_server/graph/graph.cpp
+++ cs426_graph_server/graph/graph.cpp
@@ -12,13 +12,23 @@ uint64_t graph::remove_edge(uint64_t node_a_id, uint64_t node_b_id){
 }
 bool graph::get_node(uint64_t node_id){
 //    return nodes.enum_keys().contains(node_id);
-    retun nodes.find(node_id) != nodes.end;
+    return nodes.find(node_id) != nodes.end();
 }
-uint64_t graph::get_edge(uint64_t node_a_id, uint64_t node_b_id){
-    
+pair<bool,bool> graph::get_edge(uint64_t node_a_id, uint64_t node_b_id){
+    bool node_a_exist = FALSE;
+    bool node_b_exist = FALSE;
+    if (nodes.find(node_a_id)!= nodes.end()) {
+        node_a_exist = TRUE;
+    }
+    if (nodes.find(node_b_id) != nodes.end()) {
+        node_b_exist = TRUE;
+    }
+    return pair<node_a_exist, node_b_exist>;
 }
-uint64_t graph::get_neighbors(uint64_t node_id){
-
+unordered_set<node> graph::get_neighbors(uint64_t node_id){
+    if (get_node(node_id)) {
+        return nodes.at(node_id);
+    }
 }
 uint64_t graph::shortest_path(uint64_t node_a_id, uint64_t node_b_id){
 
--- cs426_graph_server/graph/graph.h
+++ cs426_graph_server/graph/graph.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../library/mongoose.h"
 #include <vector>
+#include <unordered_set>
 #include <unordered_map>
 #include <utility>
 
@@ -8,14 +9,14 @@ using namespace std;
 
 struct node{
 	uint64_t id;
-	vector<node*> neighbors;
+	unordered_set<node*> neighbors;
     node(uint64_t i){
         this->id=i;
     }
 };
 
 class graph{
-public:
+private:
     unordered_map<uint64_t, node*> nodes;
 public:
 	uint64_t add_node(uint64_t node_id);
@@ -23,7 +24,7 @@ public:
 	uint64_t remove_node(uint64_t node_id);
 	uint64_t remove_edge(uint64_t node_a_id, uint64_t node_b_id);
 	bool get_node(uint64_t node_id);
-	uint64_t get_edge(uint64_t node_a_id, uint64_t node_b_id);
-	uint64_t get_neighbors(uint64_t node_id);
+	pair<bool, bool> get_edge(uint64_t node_a_id, uint64_t node_b_id);
+	unordered_set<node> get_neighbors(uint64_t node_id);
 	uint64_t shortest_path(uint64_t node_a_id, uint64_t node_b_id);
 };
