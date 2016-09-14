3001517c781440e841b5a2bdf8e397efe66a988d
sinexu <sx8@uw.edu>
2016-09-13 21:25:59 -0400

    modify get_edge to be returning boolean indicating whether an edge exist modify get_neighbors add implementation for shortest_path using bfs since the weight for each edge is one, no need to use Dijkstra. :) Me go shower than League of legends :D

--- cs426_graph_server/graph/graph.h
+++ cs426_graph_server/graph/graph.h
@@ -4,6 +4,7 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <utility>
+#include <queue>
 
 using namespace std;
 
@@ -24,7 +25,22 @@ public:
 	uint64_t remove_node(uint64_t node_id);
 	uint64_t remove_edge(uint64_t node_a_id, uint64_t node_b_id);
 	bool get_node(uint64_t node_id);
+    /** 
+       Returns a pair of boolean.
+       First boolean indicates if the edge exists in the graph.
+       Second boolean returns true if both nodes exist in the graph;
+                              false if any of them doesn't exist in the graph.
+     */
 	pair<bool, bool> get_edge(uint64_t node_a_id, uint64_t node_b_id);
-	unordered_set<node> get_neighbors(uint64_t node_id);
-	uint64_t shortest_path(uint64_t node_a_id, uint64_t node_b_id);
+    /**
+        Returns a set of nodes (the neighbors of the specific node).
+     */
+    unordered_set<node*> get_neighbors(uint64_t node_id);
+    /**
+        Returns a pair of int and boolean.
+        int stands for the shortest path length, if returned -1, then there is no path betwwen node a and b.
+        Boolean returns true if both nodes exist in the graph;
+                        false if any of them doesn't exist in the graph.
+     */
+	pair<uint64_t, bool> shortest_path(uint64_t node_a_id, uint64_t node_b_id);
 };
